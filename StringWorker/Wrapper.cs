534494019fc99d8aee7f187faeab46dbab538ff2
stefan2595 <40332880+stefan2595@users.noreply.github.com>
2019-04-07 11:36:00 +0300

    Add implementation

---
+++ StringWorker/Wrapper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StringWorker
+{
+    public class Wrapper
+    {
+        public Wrapper(string input)
+        {
+            if (input.Length != 0)
+            {
+                internalString = input;
+            }
+            else
+            {
+                internalString = "-1";
+            }
+        }
+
+        public string getValue()
+        {
+            return internalString;
+        }
+
+        public int append(string input)
+        {
+            internalString += input;
+            return internalString.Length;
+        }
+
+        public int prepend(string input)
+        {
+            internalString = internalString.Insert(0, input);
+            return internalString.Length;
+        }
+
+
+        public int removeCharAt(int position)
+        {
+            internalString = internalString.Remove(position, 1);
+            return internalString.Length;
+        }
+
+        public int emptyString()
+        {
+            internalString = "";
+            return internalString.Length;
+        }
+
+        private string internalString;
+    }
+}
---
+++ WrapperTests/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using StringWorker;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WrapperTest
+{
+    [TestClass]
+    public class WrapperTestClass
+    {
+        [TestMethod]
+        public void append_validString_validLength()
+        {
+            string testString = "test";
+            string toAppend = "123";
+            Wrapper wrapper = new Wrapper(testString);
+            int result = wrapper.append(toAppend);
+            Assert.AreEqual(result, 7);
+        }
+
+        [TestMethod]
+        public void append_validString_validInternalString()
+        {
+            string testString = "test";
+            string toAppend = "123";
+            Wrapper wrapper = new Wrapper(testString);
+            wrapper.append(toAppend);
+            string result = wrapper.getValue();
+            Assert.AreEqual(result, "test123");
+        }
+
+        [TestMethod]
+        public void prepend_validString_validLength()
+        {
+            string testString = "test";
+            string toAppend = "123";
+            Wrapper wrapper = new Wrapper(testString);
+            int result = wrapper.prepend(toAppend);
+            Assert.AreEqual(result, 7);
+        }
+
+        [TestMethod]
+        public void prepend_validString_validInternalString()
+        {
+            string testString = "test";
+            string toAppend = "123";
+            Wrapper wrapper = new Wrapper(testString);
+            wrapper.prepend(toAppend);
+            string result = wrapper.getValue();
+            Assert.AreEqual(result, "123test");
+        }
+
+        [TestMethod]
+        public void removeCharAt_validPosition_validLength()
+        {
+            string testString = "test";
+            int toRemove = 1;
+            Wrapper wrapper = new Wrapper(testString);
+            int result = wrapper.removeCharAt(toRemove);
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void removeCharAt_validPosition_validInternalString()
+        {
+            string testString = "test";
+            int toRemove = 1;
+            Wrapper wrapper = new Wrapper(testString);
+            wrapper.removeCharAt(toRemove);
+            string result = wrapper.getValue();
+            Assert.AreEqual(result, "tst");
+        }
+
+        [TestMethod]
+        public void emptyString_always_0Length()
+        {
+            string testString = "test";
+            Wrapper wrapper = new Wrapper(testString);
+            int result = wrapper.emptyString();
+            Assert.AreEqual(result, 0);
+        }
+    }
+}
